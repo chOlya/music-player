ebe8129007608676b8e51d9cd30461ff440a1198
chOlya <Chvyrova.o@mail.ru>
2023-11-20 20:38:26 +0300

    add song title

--- frontend/src/components/AudioComponent/AudioComponent.tsx
+++ frontend/src/components/AudioComponent/AudioComponent.tsx
@@ -4,29 +4,35 @@ import {AudioPlayerContext} from '../AudioPlayer/AudioPlayer';
 
 const AudioComponent: React.FC = () => {
     const context = React.useContext(AudioPlayerContext);
-    const { state: {progressBarRef, currentSong, songRef}, actions: {setIsPlaying, setDuration}} = context;
-    
+    const {state: {progressBarRef, currentSong, songRef}, actions: {setIsPlaying, setDuration}} = context;
+
+    const songTitle = `${currentSong.author} - ${currentSong.title}`;
+
     const onSongEnd = (): void => {
         setIsPlaying(false);
     };
-    
+
     const onTimeUpdate = (): void => {
         const changeProgress = () => {
             progressBarRef.current.value = songRef.current.currentTime;
         };
-        
+
         window.requestAnimationFrame(changeProgress);
     };
-    
+
     return (
-        <audio
-            src={currentSong.songSrc}
-            controls
-            ref={songRef}
-            onLoadedMetadata={() => setDuration(songRef.current.duration)}
-            onEnded={onSongEnd}
-            onTimeUpdate={onTimeUpdate}
-        />
+        <div className="audio-component">
+            <audio
+                src={currentSong.songSrc}
+                ref={songRef}
+                onLoadedMetadata={() => setDuration(songRef.current.duration)}
+                onEnded={onSongEnd}
+                onTimeUpdate={onTimeUpdate}
+            />
+            <div className="audio-component__song-name">
+                {songTitle}
+            </div>
+        </div>
     );
 };
 
