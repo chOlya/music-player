73f476f72871d6c1428a39d775894e4833cb945d
chOlya <Chvyrova.o@mail.ru>
2023-11-20 21:29:53 +0300

    add more music

--- frontend/src/objects/music.ts
+++ frontend/src/objects/music.ts
@@ -1,5 +1,10 @@
 import piano from '../music/pianomoment.mp3';
+import creative from '../music/creativeminds.mp3';
+import tenderness from '../music/tenderness.mp3';
+
 import pianoCover from "../images/covers/pianomoment.jpeg"
+import creativeCover from "../images/covers/creativeminds.webp"
+import tendernessCover from "../images/covers/tenderness.webp"
 
 export interface Song {
     title: string;
@@ -14,5 +19,17 @@ export const music: Array<Song> = [
         author: 'Benjamin Tissot',
         songSrc: piano,
         songCover: pianoCover,
+    },
+    {
+        title: 'Creative Minds',
+        author: 'Benjamin Tissot',
+        songSrc: creative,
+        songCover: creativeCover,
+    },
+    {
+        title: 'Tenderness',
+        author: 'Benjamin Tissot',
+        songSrc: tenderness,
+        songCover: tendernessCover,
     },
 ];
