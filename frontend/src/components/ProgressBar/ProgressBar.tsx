e5f9c8f09de4cfef29aabe7f0fd55c6957c882a5
chOlya <Chvyrova.o@mail.ru>
2023-11-20 22:36:20 +0300

    hide progress bar on pause

--- frontend/src/components/ProgressBar/ProgressBar.tsx
+++ frontend/src/components/ProgressBar/ProgressBar.tsx
@@ -3,11 +3,13 @@ import {AudioPlayerContext} from '../AudioPlayer/AudioPlayer';
 
 const ProgressBar: React.FC = () => {
     const context = React.useContext(AudioPlayerContext);
-    const { state: {progressBarRef, duration, songRef} } = context;
+    const { state: {progressBarRef, duration, songRef, isPlaying} } = context;
     
     const handleProgressChange = (): void => {
         songRef.current.currentTime = progressBarRef.current.value;
     };
+    
+    const opacity = isPlaying ? 1 : 0;
     
     return (
         <div className="progress-bar">
@@ -18,6 +20,7 @@ const ProgressBar: React.FC = () => {
                 defaultValue={0}
                 max={duration}
                 className="progress-bar__range input-range"
+                style={{ opacity }}
             />
         </div>
     );
